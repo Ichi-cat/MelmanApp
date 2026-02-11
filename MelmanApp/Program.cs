859c3627681a829d7bd6f76ec7b7e7aecfc89931
Stanislav Holoborodyi <op_sholoborodyi@ennov.com>
2026-02-11 19:20:01 +0100

    начало

--- MelmanApp/Program.cs
+++ MelmanApp/Program.cs
@@ -31,25 +31,81 @@ app.MapGet("/healthz", () =>
 });
 app.MapPost("/combat", (GameRequest model) =>
 {
+    var actions = new List<GameAction>();
+    var money = model.PlayerTower.Resources;
 
-    var actions = new List<GameAction>
+    if(money >= 50 && model.PlayerTower.Level == 1)
     {
-        new GameAction
-        {
-            Type = "armor",
-            Amount = 5
-        },
-        new GameAction
-        {
-            Type = "attack",
-            TargetId = 102,
-            TroopCount = 20
-        },
-        new GameAction
+        actions.Add(new GameAction
         {
             Type = "upgrade"
-        }
-    };
+        });
+        money -= 50;
+    }
+
+    if (money >= 88 && model.PlayerTower.Level == 2)
+    {
+        actions.Add(new GameAction
+        {
+            Type = "upgrade"
+        });
+
+        money -= 88;
+    }
+
+    if (money >= 153 && model.PlayerTower.Level == 3)
+    {
+        actions.Add(new GameAction
+        {
+            Type = "upgrade"
+        });
+
+        money -= 153;
+    }
+
+    if (money >= 268 && model.PlayerTower.Level == 4)
+    {
+        actions.Add(new GameAction
+        {
+            Type = "upgrade"
+        });
+
+        money -= 268;
+    }
+
+    if (money >= 469 && model.PlayerTower.Level == 5)
+    {
+        actions.Add(new GameAction
+        {
+            Type = "upgrade"
+        });
+
+        money -= 469;
+    }
+
+    if (money > 0 && model.Turn > 2)
+    {
+        actions.Add(new GameAction
+        {
+            Type = "armor",
+            Amount = money / 3
+        });
+    }
+
+    //var actions = new List<GameAction>
+    //{
+    //    new GameAction
+    //    {
+    //        Type = "armor",
+    //        Amount = 5
+    //    },
+    //    new GameAction
+    //    {
+    //        Type = "attack",
+    //        TargetId = 102,
+    //        TroopCount = 20
+    //    }
+    //};
 
     return Results.Ok(actions);
 });
